588c019f79ed23dda8087fe8cc8f77a1367de796
Juan Pablo Ferrari <ferrarijuanp@gmail.com>
2015-10-07 10:43:57 -0300

    proceso automatico de backup

--- mongo-backup.sh
+++ mongo-backup.sh
@@ -1,11 +1,11 @@
-MONGO_DATABASE="m101"
-APP_NAME="M101"
+MONGO_DATABASE="db_name"
+APP_NAME="app_name"
 
 MONGO_HOST="127.0.0.1"
 MONGO_PORT="27017"
 TIMESTAMP=`date +%F-%H%M`
 MONGODUMP_PATH="/usr/bin/mongodump"
-BACKUPS_DIR="/home/kanaima/backups/$APP_NAME"
+BACKUPS_DIR="/home/usuario/backups/$APP_NAME"
 BACKUP_NAME="$APP_NAME-$TIMESTAMP"
 
 # mongo admin --eval "printjson(db.fsyncLock())"
@@ -17,4 +17,4 @@ mkdir -p $BACKUPS_DIR
 mv dump $BACKUP_NAME
 tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
 rm -rf $BACKUP_NAME
-echo "This is message body" | mail -s "This is Subject" -a $BACKUPS_DIR/$BACKUP_NAME.tgz ferrarijuanp@gmail.com
+echo "This is message body" | mail -s "This is Subject" -a $BACKUPS_DIR/$BACKUP_NAME.tgz mail@dominio.com
